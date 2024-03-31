8d9822a1309e91bea4575b35667e86c85d90c090
Pascal4Games <62812321+Pascal4Games@users.noreply.github.com>
2024-03-31 15:45:17 +0200

    update to V27

--- homewizard_climate_ws/model/climate_device_state.py
+++ homewizard_climate_ws/model/climate_device_state.py
@@ -29,7 +29,6 @@ class HomeWizardClimateDeviceState:
     cool: bool
     mute: bool
     warning: list[str]
-    lock: bool
 
 
 def default_state():
@@ -56,7 +55,6 @@ def default_state():
             "ext_current_temperature": 0,
             "ext_target_temperature": 0,
             "cool": False,
-            "lock": False,
             "mute": False,
             "warning": [],
         }
